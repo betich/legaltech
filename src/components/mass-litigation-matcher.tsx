8bc858e1cb8dd3bc4f1585a5715e799f00ba659c
betich <betich365@gmail.com>
2025-02-22 11:57:59 +0700

    change skeleton color

--- src/components/mass-litigation-matcher.tsx
+++ src/components/mass-litigation-matcher.tsx
@@ -91,15 +91,15 @@ const MassLitigationMatcher = () => {
     <div className="p-4 border rounded-lg space-y-4">
       <div className="flex justify-between items-start">
         <div className="space-y-2">
-          <Skeleton className="h-6 w-64" />
-          <Skeleton className="h-4 w-40" />
+          <Skeleton className="bg-gray-100 h-6 w-64" />
+          <Skeleton className="bg-gray-100 h-4 w-40" />
         </div>
         <Skeleton className="h-6 w-6" />
       </div>
       <div className="space-y-2">
-        <Skeleton className="h-4 w-32" />
-        <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-full" />
+        <Skeleton className="bg-gray-100 h-4 w-32" />
+        <Skeleton className="bg-gray-100 h-4 w-full" />
+        <Skeleton className="bg-gray-100 h-4 w-full" />
       </div>
     </div>
   );
