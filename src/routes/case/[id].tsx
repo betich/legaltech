350c5912a26d57d1bb72edde659e1c17ad18490b
betich <betich365@gmail.com>
2025-02-22 08:17:39 +0700

    remove

--- src/routes/case/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function CaseIdPage() {
-  const { id } = useParams(); // Get the dynamic parameter
-
-  return <h1>Dynamic Page for ID: {id}</h1>;
-}
--- src/routes/lawyer/index.tsx
+++
@@ -1,9 +0,0 @@
-import ClaimDashboard from "@/components/claim-dashboard";
-
-export default function Lawyer() {
-  return (
-    <div>
-      <ClaimDashboard />
-    </div>
-  );
-}
